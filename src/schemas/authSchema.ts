ed9a46fc5f8d63e7485e50ff35ac424fc817fb99
Mateus Brasil <Mateus-code-98>
2022-08-03 09:09:15 -0300

    Preparando revisão de código

--- src/schemas/authSchema.ts
+++
@@ -1,7 +0,0 @@
-import joi from "joi";
-import { userData } from "../services/authServices";
-
-export const userSchema = joi.object<userData>({
-  email: joi.string().required(),
-  password: joi.string().min(10).required(),
-});
